ff1cb299ae188379fa0b570e06507fa604d81a1c
gustavotremontsr <gustavotremontsr@gmail.com>
2023-10-09 19:26:01 +0200

    fix: modified some names

--- src/controllers/models/index.ts
+++ src/controllers/models/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/comma-dangle */
 import { deleteModel } from './deleteModel.controller'
-import { getAllModels } from './getAllModels.controllers'
+import { getAllModels } from './getAllModels.controller'
 import { getModel } from './getModel.controller'
 import { updateModel } from './updateModel.controller'
 
@@ -8,5 +8,5 @@ export const modelController = {
   getAllModels,
   getModel,
   updateModel,
-  deleteModel,
+  deleteModel
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import { Express } from 'express'
-import { projectRoutes } from './projects.routes'
+import { projectRoutes } from './projects.route'
 import { modelRoutes } from './models.route'
 
 export const routes = (app: Express): void => {
-  app.use('/api/project', projectRoutes)
-  app.use('/api/model', modelRoutes)
+  app.use('/api/projects', projectRoutes)
+  app.use('/api/models', modelRoutes)
+
+  app.use('*', (_req, res) => {
+    res.status(404).json({
+      message: 'invalid endpoint'
+    })
+  })
 }
